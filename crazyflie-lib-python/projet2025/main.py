35f53a26932def9236efc7a99fb182364c3def5a
emiliesdgh <emilie.grandjean@epfl.ch>
2025-05-08 13:28:37 +0200

    motion_planning 3D okey, still trying to disyplay the 2D version with the gates to see if modify 3D to add gates as well

--- crazyflie-lib-python/projet2025/main.py
+++ crazyflie-lib-python/projet2025/main.py
@@ -3,42 +3,47 @@ import cv2
 import time 
 
 
-from map import OccupancyGrid as OG
+from gates_map import GatesMap as GM
+from motion_planning import MotionPlanner3D as MP
 
 
 if __name__ == "__main__":
     # Initialize the occupancy grid
-    occupancy_grid = OG()
-    # # Set the gates in the occupancy grid
-    # occupancy_grid.set_gates([10, 10, 0, 0], [100, 100, 0, 0], [40, 20, 0, 45], [120, 35, 0, 25], [100, 90, 0, 90], [50, 120, 0, 135])
-    # # Display the occupancy grid
-    # while(True):
-    #     occupancy_grid.display_map()
+    gate_map = GM()
 
-    # Create the occupancy grid
+    # Set start, end, and gate positions (x, y, z, theta, gate size)
+    # all positions are given in meters and theta in degrees
+    start = np.array([1.5, 1.5, 0, 0.6])
+    end = np.array([1.5, 1.5, 1, 0.6])
 
-    # Set start, end, and gate positions (x, y, z, theta, extra)
-    start = np.array([75, 25, 1, 0])
-    end = np.array([75, 25, 1, 0])
-    gate1 = np.array([100, 50, 0.5, 45, 0])
-    gate2 = np.array([100, 100, 0.75, 135, 0])
-    gate3 = np.array([50, 100, 1, 225, 0])
-    gate4 = np.array([50, 50, 0.75, 315, 0])
+    gate1 = np.array([2.25, 2.05, 0.8, 90, 0.6])
+    gate2 = np.array([3.6, 1.5, 1.2, 180, 0.6])
+
+    gate3 = np.array([2.3, 0.2, 1.7, 270, 0.6])
+    gate4 = np.array([1.45, 0.8, 1.6, 0, 0.6])
+
+    waypoints = (start[:3], gate1[:3], gate2[:3], gate3[:3], gate4[:3], end[:3])
+
+    # Create the motion planner
+    motion_planner = MP(waypoints)
+
+    motion_planner.plot(waypoints, motion_planner.trajectory_setpoints)
+
+    # # print the waypoints
+    # print("Waypoints:", waypoints) 
+    # # print the interpolated trajectory setpoints
+    # print("Interpolated trajectory setpoints:", motion_planner.trajectory_setpoints)
 
     # Set the gate and waypoint data
-    occupancy_grid.set_gates(start, end, gate1, gate2, gate3, gate4)
-    occupancy_grid.plan_through_gates()
+    gate_map.set_gates(start, end, gate1, gate2, gate3, gate4)
+    # gate_map.set_optimal_path(motion_planner.trajectory_setpoints)
+    # gate_map.set_optimal_path([pt[:2] for pt in motion_planner.trajectory_setpoints])
 
-    # Optionally: add obstacles to the map
-    # og.grid[50:70, 50:70] = 255
-
-    # Run path planning
-    # path = occupancy_grid.path_planning()
-
+    scaled_path = [(x * 10, y * 10) for x, y, *_ in motion_planner.trajectory_setpoints]
+    gate_map.set_optimal_path(scaled_path)
     # Display result in a loop (until key press)
     while True:
-        occupancy_grid.display_map()
-        occupancy_grid.display_3d_map()
+        gate_map.display_map()
         if cv2.waitKey(50) & 0xFF == ord('q'):
             break
 
